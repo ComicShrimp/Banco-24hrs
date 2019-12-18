fe243c1572dee786744ee0f239f54c82a7b31772
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-12-18 18:21:20 -0300

    Termina a tela de login e realiza chamada api

--- frontend/src/javascript/login.js
+++ frontend/src/javascript/login.js
@@ -1,19 +1,24 @@
 const api = require("../services/api")
+require("dotenv").config({
+  path: ".env"
+})
 
 async function fazLogin() {
   numeroDaConta = document.getElementById("numeroDaConta")
   senha = document.getElementById("senha")
 
-  window.setTimeout(2000)
-
   if (numeroDaConta.value.toString().startsWith("5")) {
-    const response = await api.get(
-      process.env.BANCO_DO_BRASIL_URL + numeroDaConta.value,
-      {
-        senha: senha.value
-      }
-    )
-    alert("loco")
+    try {
+      const response = await api.get(
+        process.env.BANCO_DO_BRASIL_URL + numeroDaConta.value,
+        {
+          senha: senha.value
+        }
+      )
+      alert(response.data.nome)
+    } catch (error) {
+      console.log("Deu Errado" + error)
+    }
   } else if (numeroDaConta.value.toString().startsWith("4")) {
   } else if (numeroDaConta.value.toString().startsWith("3")) {
   } else {
